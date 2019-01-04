2160dc6f5a0786d7e56b549e424713f44bb73997
Cedric De Haes <cedric.dehaes@student.uantwerpen.be>
2019-01-04 11:12:54 +0100

    Made the 3*3 grid into a 5*5 grid for the naive gpu implementation

--- gpu/GameOfLife.cpp
+++ gpu/GameOfLife.cpp
@@ -64,6 +64,51 @@ std::string GameOfLife::Compile() const {
         result += "    }";
     }
 
+
+    if(weights[0][0] != 0 && weights[1][0]!= 0 && weights[2][0] != 0 && weights[3][0] != 0 && weights[4][0] != 0){
+        result += "if(get_global_id(0) > 1){\n";
+        if(weights[0][0] != 0){
+            result += "if(get_global_id(1)>1){\n"
+                      "neighbours += ";
+            if(weights[0][0]!= 1){
+                result += std::to_string(weights[0][0]) + "* ";
+            }
+            result += "(data[(get_global_id(0)-2)+(get_global_id(1)-2) * get_global_size(0)]);}";
+        }
+        if(weights[1][0] != 0){
+            result += "if(get_global_id(1)>0){\n"
+                      "neighbours += ";
+            if(weights[1][0]!= 1){
+                result += std::to_string(weights[1][0]) + "* ";
+            }
+            result += "(data[(get_global_id(0)-2)+(get_global_id(1)-1) * get_global_size(0)]);}";
+        }
+        if(weights[2][0] != 0){
+            result += "neighbours += ";
+            if(weights[2][0]!= 1){
+                result += std::to_string(weights[2][0]) + "* ";
+            }
+            result += "(data[(get_global_id(0)-2)+(get_global_id(1)) * get_global_size(0)]);";
+        }
+        if(weights[3][0]!= 0){
+            result += "if(get_global_id(1)<get_global_size(1)-1){\n"
+                      "neighbours += ";
+            if(weights[3][0]!= 1){
+                result += std::to_string(weights[3][0]) + "* ";
+            }
+            result += "(data[(get_global_id(0)-2)+(get_global_id(1)+1) * get_global_size(0)]);}";
+        }
+        if(weights[4][0]!= 0){
+            result += "if(get_global_id(1)<get_global_size(1)-2){\n"
+                      "neighbours += ";
+            if(weights[4][0]!= 1){
+                result += std::to_string(weights[4][0]) + "* ";
+            }
+            result += "(data[(get_global_id(0)-2)+(get_global_id(1)+2) * get_global_size(0)]);}";
+        }
+        result += "}\n";
+    }
+
     if(weights[2][3]!= 0 && weights[1][3]!=0 && weights[3][3]!=0 && dimensions > 1){
         result += "if(get_global_id(0) != get_global_size(0)-1){\n";
         if(weights[2][3] != 0)
@@ -101,6 +146,51 @@ std::string GameOfLife::Compile() const {
         result += "    }";
     }
 
+    if(weights[0][4] != 0 && weights[1][4]!= 0 && weights[2][4] != 0 && weights[3][4] != 0 && weights[4][4] != 0){
+        result += "if(get_global_id(0) < get_global_size(0)-2){\n";
+        if(weights[0][4] != 0){
+            result += "if(get_global_id(1)>1){\n"
+                      "neighbours += ";
+            if(weights[0][4]!= 1){
+                result += std::to_string(weights[0][4]) + "* ";
+            }
+            result += "(data[(get_global_id(0)+2)+(get_global_id(1)-2) * get_global_size(0)]);}";
+        }
+        if(weights[1][4] != 0){
+            result += "if(get_global_id(1)>0){\n"
+                      "neighbours += ";
+            if(weights[1][4]!= 1){
+                result += std::to_string(weights[1][4]) + "* ";
+            }
+            result += "(data[(get_global_id(0)+2)+(get_global_id(1)-1) * get_global_size(0)]);}";
+        }
+        if(weights[2][4] != 0){
+            result += "neighbours += ";
+            if(weights[2][4]!= 1){
+                result += std::to_string(weights[2][4]) + "* ";
+            }
+            result += "(data[(get_global_id(0)+2)+(get_global_id(1)) * get_global_size(0)]);";
+        }
+        if(weights[3][4]!= 0){
+            result += "if(get_global_id(1)<get_global_size(1)-1){\n"
+                      "neighbours += ";
+            if(weights[3][4]!= 1){
+                result += std::to_string(weights[3][4]) + "* ";
+            }
+            result += "(data[(get_global_id(0)+2)+(get_global_id(1)+1) * get_global_size(0)]);}";
+        }
+        if(weights[4][4]!= 0){
+            result += "if(get_global_id(1)<get_global_size(1)-2){\n"
+                      "neighbours += ";
+            if(weights[4][4]!= 1){
+                result += std::to_string(weights[4][4]) + "* ";
+            }
+            result += "(data[(get_global_id(0)+2)+(get_global_id(1)+2) * get_global_size(0)]);}";
+        }
+        result += "}\n";
+    }
+
+
     if (weights[1][2] != 0){
         // Top
         result += "if(get_global_id(1) != 0){\n"
@@ -111,6 +201,35 @@ std::string GameOfLife::Compile() const {
         result += "(data[get_global_id(0)+(get_global_id(1)-1) * get_global_size(0)]);\n"
                   "    }";
     }
+
+    if(weights[0][1]!=0 && weights[0][2] != 0 && weights[0][3] != 0){
+        result += "if(get_global_id(1) > 1){";
+        if(weights[0][1]!= 0){
+            result += "if(get_global_id(0)> 0){"
+                      "neighbours +=";
+            if(weights[0][1] != 1){
+                result += std::to_string(weights[0][1]) + "* ";
+            }
+            result += "(data[(get_global_id(0)-1)+(get_global_id(1)-2) * get_global_size(0)]);}";
+        }
+        if(weights[0][2] != 0){
+            result +="neighbours +=";
+            if(weights[0][2] != 1){
+                result += std::to_string(weights[0][2]) + "* ";
+            }
+            result += "(data[(get_global_id(0))+(get_global_id(1)-2) * get_global_size(0)]);";
+        }
+        if(weights[0][3]!= 0){
+            result += "if(get_global_id(0)< get_global_size(0)-1){"
+                      "neighbours +=";
+            if(weights[0][3] != 1){
+                result += std::to_string(weights[0][3]) + "* ";
+            }
+            result += "(data[(get_global_id(0)+1)+(get_global_id(1)-2) * get_global_size(0)]);}";
+        }
+        result += "}";
+    }
+
     if (weights[3][2] != 0){
         //Bot
         result += "if(get_global_id(1) != get_global_size(1)-1){\n"
@@ -123,6 +242,33 @@ std::string GameOfLife::Compile() const {
 
     }
 
+    if(weights[4][1]!=0 && weights[4][2] != 0 && weights[4][3] != 0){
+        result += "if(get_global_id(1) < get_global_size(1)-2){";
+        if(weights[4][1]!= 0){
+            result += "if(get_global_id(0)> 0){"
+                      "neighbours +=";
+            if(weights[4][1] != 1){
+                result += std::to_string(weights[4][1]) + "* ";
+            }
+            result += "(data[(get_global_id(0)-1)+(get_global_id(1)+2) * get_global_size(0)]);}";
+        }
+        if(weights[4][2] != 0){
+            result +="neighbours +=";
+            if(weights[4][2] != 1){
+                result += std::to_string(weights[4][2]) + "* ";
+            }
+            result += "(data[(get_global_id(0))+(get_global_id(1)+2) * get_global_size(0)]);";
+        }
+        if(weights[4][3]!= 0){
+            result += "if(get_global_id(0)< get_global_size(0)-1){"
+                      "neighbours +=";
+            if(weights[4][3] != 1){
+                result += std::to_string(weights[4][3]) + "* ";
+            }
+            result += "(data[(get_global_id(0)+1)+(get_global_id(1)+2) * get_global_size(0)]);}";
+        }
+        result += "}";
+    }
 
     result += "myType state = data[id];\n"
               "\n"
